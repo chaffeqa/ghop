2c7f6b2e90335176852d2e575ed41b2bb2949951
Quinn <quinn@sportshubtech.com>
2018-08-25 21:29:59 -0400

    Styles in for resources

--- src/components/resources.jsx
+++ src/components/resources.jsx
@@ -19,6 +19,18 @@ const resources = [
     imageUrl: 'https://www.ihopkc.org/apple-touch-icon.png?v=ngGBw47L8y',
     url: `https://www.ihopkc.org/prayerroom/`,
   },
+  {
+    title: 'Prayer Furnaces',
+    description: `The mission is to plant a sustainable furnace of 24-7-365 worship, prayer and supernatural explosive outreach, releasing a sound of indigenous, vertical, creative worship in every community, people group, city and nation in the world.`,
+    imageUrl: 'https://burn247.s3.amazonaws.com/assets/burn247_logo-28de8a0e68d7bd78fc7e474d282cad8b.png',
+    url: `https://www.burn24-7.com/`,
+  },
+  {
+    title: 'iHop Sermons',
+    description: `International House of Prayer is based out of Kansas City.  24/7 streaming prayer and worship!`,
+    imageUrl: 'https://www.ihopkc.org/apple-touch-icon.png?v=ngGBw47L8y',
+    url: `https://www.ihopkc.org/prayerroom/`,
+  },
 ]
 
 const Resources = () => (
